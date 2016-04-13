19f01e5e3645a95297a39e79fcd9ff790d973833
Avl <alina.vlas@gmail.com>
2016-04-13 22:39:43 +0200

    Implement Drawer filled by ListWiew

--- src/com/formation/activities/MainActivity.java
+++ src/com/formation/activities/MainActivity.java
@@ -1,60 +1,97 @@
 package com.formation.activities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
-
+	private List<String> drawerTitles;
+	private DrawerLayout drawerLayout;
+	private ListView drawerList;
+	private CharSequence title;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_drawer_list);
 
-		Button mirrorTextBtn = (Button) findViewById(R.id.button_mirrorText);
-		mirrorTextBtn.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent mirrorTextActivity = new Intent(MainActivity.this, MirrorTextActivity.class);
-				startActivity(mirrorTextActivity);
-			}
-		});
+		initDrawerList();
 
-		Button formBtn = (Button) findViewById(R.id.button_form);
-		formBtn.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent formActivity = new Intent(MainActivity.this, FormActivity.class);
-				startActivity(formActivity);
-			}
-		});
+		title = getTitle();
+		drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		drawerList = (ListView) findViewById(R.id.left_drawer);
 
-		Button usersBtn = (Button) findViewById(R.id.button_users);
-		usersBtn.setOnClickListener(new View.OnClickListener() {
+		drawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_item, drawerTitles));
+		drawerList.setOnItemClickListener(new ListView.OnItemClickListener() {
 			@Override
-			public void onClick(View v) {
-				Intent usersActivity = new Intent(MainActivity.this, UsersActivity.class);
-				startActivity(usersActivity);
-			}
-		});
-
-		Button contactsBtn = (Button) findViewById(R.id.button_contacts);
-		contactsBtn.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent contactsActivity = new Intent(MainActivity.this, ContactsActivity.class);
-				startActivity(contactsActivity);
+			public void onItemClick(AdapterView parent, View view, int position, long id) {
+				selectItem(position);
 			}
 		});
 	}
 
+	private void initDrawerList() {
+		drawerTitles = new ArrayList<String>();
+		drawerTitles.add(getString(R.string.mirror_text));
+		drawerTitles.add(getString(R.string.form));
+		drawerTitles.add(getString(R.string.users));
+		drawerTitles.add(getString(R.string.contacts));
+	}
+
+	@Override
+	public void setTitle(CharSequence title) {
+		this.title = title;
+		getActionBar().setTitle(this.title);
+	}
+
+	private void selectItem(int position) {
+		switch (position) {
+			case 0:
+				Intent mirrorTextActivity = new Intent(MainActivity.this, MirrorTextActivity.class);
+				startActivity(mirrorTextActivity);
+				break;
+			case 1:
+				Intent formActivity = new Intent(MainActivity.this, FormActivity.class);
+				startActivity(formActivity);
+				break;
+			case 2:
+				Intent usersActivity = new Intent(MainActivity.this, UsersActivity.class);
+				startActivity(usersActivity);
+				break;
+			case 3:
+				Intent contactsActivity = new Intent(MainActivity.this, ContactsActivity.class);
+				startActivity(contactsActivity);
+				break;
+		}
+
+		// Highlight the selected item, update the title, and close the drawer
+		drawerList.setItemChecked(position, true);
+		setTitle(drawerTitles.get(position));
+		drawerLayout.closeDrawer(drawerList);
+	}
+	
+	@Override
+	public void onBackPressed() {
+		if (getFragmentManager().getBackStackEntryCount() > 0) {
+			getFragmentManager().popBackStack();
+		} else {
+			super.onBackPressed();
+		}
+	}
+	
 	protected void onPause() {
 		super.onPause();
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_drawer_list);
 	}
 
 	@Override
